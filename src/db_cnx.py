049b5515e4227ace63f9f517f11745191229f0fc
Sean Davis <mdavis92001@hotmail.com>
2024-07-18 18:06:21 -0400

    Made the db connector module

---
+++ src/db_cnx.py
@@ -0,0 +1,57 @@
+# db_cnx
+# This is the module that handles db connections. Refactor this for your specific db
+
+from pymongo import MongoClient
+import json
+
+class db_connection():
+    def __init__(self):
+        # Possibly need to refactor to keep the client & database variables private
+        try:
+            self.client = MongoClient()
+        except Exception as e:
+            print("Couldn't connect to mongodb. Please make sure mongodb is running, then restart the application. " + e)
+        try:
+            self.db = self.client.get_database("npp")
+        except Exception as e:
+            print("Couldn't connect to npp database. Please create the database, then restart the application. " + e)
+        self.users = self.db.users
+        self.inventory = self.db.inventory
+        self.orders = self.db.orders
+
+    def db_search(self, searchable, collection):
+        match collection.lower():
+            case "users":
+                return self.users.find(searchable)
+            case "inventory":
+                return self.inventory.find(searchable)
+            case "orders":
+                return self.orders.find(searchable)
+
+    def db_insert(self, insertable, collection):
+        match collection.lower():
+            case "users":
+                return self.users.insert_one(insertable)
+            case "inventory":
+                return self.inventory.insert_one(insertable)
+            case "orders":
+                return self.orders.insert_one(insertable)
+    
+    def db_update(self, updateable, collection):
+        match collection.lower():
+            case "users":
+                return self.users.update_one(updateable)
+            case "inventory":
+                return self.inventory.update_one(updateable)
+            case "orders":
+                return self.orders.update_one(updateable)
+
+    def db_delete(self, deleateble, collection):
+        match collection.lower():
+            case "users":
+                return self.users.delete_one(deleateble)
+            case "inventory":
+                return self.inventory.delete_one(deleateble)
+            case "orders":
+                return self.orders.delete_one(deleateble)
+    
